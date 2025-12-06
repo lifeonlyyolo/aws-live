5395b1af49fafb147bdc9325c2dead46c748fa19
lifeonlyyolo <terryong8800@gmail.com>
2025-12-06 16:01:12 +0800

    Update database connection details in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "ec2-54-167-77-118.compute-1.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "24PMD02200"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "yl-s3-bucket123"
 customregion = "us-east-1"
 
